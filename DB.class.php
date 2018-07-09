448e743d0f6410ababab59675fc1fe79ff3b7a8a
pairavanvvl <36874538+pairavanvvl@users.noreply.github.com>
2018-07-09 19:21:38 +0530

    Add files via upload

--- DB.class.php
+++ DB.class.php
@@ -2,106 +2,119 @@
 	/**
      * @param Library for CRUD operation
      *********************************************************************************
-     * @param v1.0 Beta
+     * @param v2.0 Beta
      *********************************************************************************
      * @param Copyright © 2018 PyRu Inc.
      */
 	class DB{
-		public static $tableColumns; //imploaded values
-		public static $values_keys;
-		public static $values_values;
-		public static $newValues;
-		public static $response = [];
+		private static $tableColumns; //splice first column i.e primary field
+		private $db;
+		private $table;
+		private $data;
+		private $encrypt 	= 0;
+		private $encryptKey = 'DEFAULT';
+		private $dataKeys;
+		private $dataValues;
+		private $newValues;
+		private $response 	= [];
 
-		public static function getTableColumns($db, $table){
-			$query = 'SHOW COLUMNS FROM '.$table;
-			$con = $db->prepare($query);
+		public function connect($db){
+			$this->db = $db;
+		}
+
+		public function table($table){
+			$this->table = $table;
+		}
+
+		public static function getTableColumns($table){
+			$query 			 = 'SHOW COLUMNS FROM '.$table;
+			$con 			 = $this->db->prepare($query);
 			$con->execute();
 			$getTableColumns = [];
 			while($row = $con->fetch(PDO::FETCH_ASSOC)){
 				$getTableColumns[] = ($row['Field']);
 			}
 			array_splice($getTableColumns, 0, 1);
-			return $getTableColumns;
+			return self::$tableColumns = $getTableColumns;
 		}
 
-		/*public static function create($db, $table, $values){
-			$tableColumns = implode(', ', self::getTableColumns($db, $table));
-			if(is_array($values)){
-				$newValues = [];
-				foreach($values as $value){
-					$newValues[] = '\''.$value.'\'';
-				}
-				$newValues = implode(',', $newValues);
+		public function data($data){
+			if(is_array($data)){
+				$this->data 	  = $data;
+				$this->dataKeys   = array_keys($data);
+				$this->dataValues = array_values($data); 
 			}
-			$query = 'INSERT INTO '.$table.'('.$tableColumns.') VALUES('.$newValues.')';
-			$con = $db->prepare($query);
-			$con1 = $con->execute();
-			echo $con1 ? 'Values Added Successfully' : 'Failed to Add';
-		}*/
-
-		public static function create($db, $table, $values){
-			if(self::isExist($db, $table, $values)['status'] != 1){
-				$query = 'INSERT INTO '.$table.'('.self::$tableColumns.') VALUES('.self::$newValues.')';
-				$con = $db->prepare($query);
-				$con1 = $con->execute();
-				if($con1)
-					return self::$response = ['status'=>'1', 'message'=>'Values Added Successfully'];
-				else
-					return self::$response = ['status'=>'0', 'message'=>'Error Occured'];
-			}else{
-				return self::$response = ['status'=>'2', 'message'=>'Already Exist'];
-			}
+			else
+				echo "data must be an array";
 		}
 
-		public static function update($db, $table, $values){
-			if(self::isExist($db, $table, $values)){
-				$len = sizeof($values);
-				$assignValues = self::$values_keys[0].' = "'.self::$values_values[0].'"';
-				$condition = self::$values_keys[0].' = "'.self::$values_values[0].'"'; 
+		public function encrypt($key){
+			$this->encrypt = 1;
+			$this->encryptKey = $key;
+		}
+
+		public function create(){
+			if($this->encrypt){
+				$dataKeys   = implode(', ', $this->dataKeys); //comma separated keys
+				$dataValues = "AES_ENCRYPT('".implode("', '$this->encryptKey'), AES_ENCRYPT('", $this->dataValues)."', '$this->encryptKey')"; //comma and quoted values
+				$query 		= 'INSERT INTO '.$this->table.'('.$dataKeys.') VALUES('.$dataValues.')';
+				$con 		= $this->db->prepare($query);
+				$con1 		= $con->execute();
+			}
+			else{
+				$dataKeys   = implode(', ', $this->dataKeys); //comma separated keys
+				$dataValues = '\''.implode('\', \'', $this->dataValues).'\''; //comma and quoted values
+				$query 		= 'INSERT INTO '.$this->table.'('.$dataKeys.') VALUES('.$dataValues.')';
+				$con 		= $this->db->prepare($query);
+				$con1 		= $con->execute();
+			}
+			if($con1)
+				return $this->response = ['status'=>'1', 'message'=>'Data Added Successfully'];
+			else
+				return $this->response = ['status'=>'0', 'message'=>'Error Occured'];
+		}
+
+		public function update(){
+			if($this->isExist()){
+				$len 			= sizeof($this->data);
+				$assignValues   = $this->dataKeys[0].' = "'.$this->dataValues[0].'"';
+				$condition 		= $this->dataKeys[0].' = "'.$this->dataValues[0].'"'; 
 				for($i=1;$i<$len;$i++){
-					$assignValues .= ', '.self::$values_keys[$i].' = "'.self::$values_values[$i].'"';
-					$condition .= ' OR '.self::$values_keys[$i].' = "'.self::$values_values[$i].'"';
+					$assignValues .= ', '.$this->dataKeys[$i].' = "'.$this->dataValues[$i].'"';
+					$condition 	  .= ' OR '.$this->dataKeys[$i].' = "'.$this->dataValues[$i].'"';
 				}
-				$query = 'UPDATE '.$table.' SET '.$assignValues.' WHERE '.$condition;
-				$con = $db->prepare($query);
-				$con1 = $con->execute();
-				return $con1 ? self::$response = ['status'=>'1', 'message'=>'Values Updated Successfully'] : self::$response = ['status'=>'0', 'message'=>'Error Occured'];
+				$query = 'UPDATE '.$this->table.' SET '.$assignValues.' WHERE '.$condition;
+				$con   = $this->db->prepare($query);
+				$con1  = $con->execute();
+				return $con1 ? $this->response = ['status'=>'1', 'message'=>'Values Updated Successfully'] : $this->$response = ['status'=>'0', 'message'=>'Error Occured'];
 			}else{
-				return self::$response = ['status'=>'2', 'message'=>'Not Found'];
+				echo "<p style='color: red;'>Data not exist</p>";
 			}
 		}
 
-		public static function isExist($db, $table, $values){
-			if(is_array($values)){
-				$newValues = [];
-				$values_keys = array_keys($values);
-				self::$values_keys = $values_keys;
-				$tableColumns = implode(', ', $values_keys);
-				self::$tableColumns = $tableColumns;
-				$values_values = array_values($values);
-				self::$values_values = $values_values;
-				$newValues = '"'.implode('"," ' , $values_values).'"';
-				self::$newValues = $newValues;
+		public function isExist(){
+			$dataKeys   	= implode(', ', $this->dataKeys); //comma separated keys
+			$len 			= sizeof($this->data);
+			$assignValues   = $this->dataKeys[0].' = "'.$this->dataValues[0].'"';
+			$condition 		= $this->dataKeys[0].' = "'.$this->dataValues[0].'"'; 
+			for($i=1;$i<$len;$i++){
+				$assignValues .= ', '.$this->dataKeys[$i].' = "'.$this->dataValues[$i].'"';
+				$condition 	  .= ' OR '.$this->dataKeys[$i].' = "'.$this->dataValues[$i].'"';
 			}
-			$query = 'SELECT '.$values_keys[0].' FROM '.$table.' WHERE '.$values_keys[0].' = \''.$values[$values_keys[0]].'\'';
-			$con = $db->prepare($query);
+			$query 		= 'SELECT '.$dataKeys.' FROM '.$this->table.' WHERE  '.$condition;
+			$con = $this->db->prepare($query);
 			$con->execute();
-			while($row = $con->fetch(PDO::FETCH_ASSOC)){
-				$rowCount = $con->rowCount();
-			}
-			if(isset($rowCount)){
-				$cond = $rowCount >= 1;
-				return $cond ? self::$response = ['status'=>'1', 'message'=>'Already Exist'] : self::$response = ['status'=>'0', 'message'=>'Error Occured'];
-			}else{
-				return self::$response = ['status'=>'0', 'message'=>'Not Found(Exist)'];
-			}
+			$rowCount = $con->rowCount();
+			if($rowCount >= 1)
+				return 1;
+			else
+				return 0;
 		}
 
-		public static function delete($db, $table, $values){
+		public function delete($db, $table, $values){
 			if(self::isExist($db, $table, $values)){
 				$condition = self::$values_keys[0].' = "'.self::$values_values[0].'"';
-				$query = 'DELETE FROM '.$table.' WHERE '.$condition;
+				$query 	= 'DELETE FROM '.$table.' WHERE '.$condition;
 				$con = $db->prepare($query);
 				$response = $con->execute();
 				if($response){
@@ -109,6 +122,26 @@
 				}else{
 					return self::$response = ['status'=>'0', 'message'=>'Error Occured'];
 				}
+			}
+		}
+
+		public function export($db, $table, $directory){
+			$con = $db->prepare('SELECT * FROM '.$table);
+			$con->execute();
+			$data = [];
+			while($row = $con->fetch(PDO::FETCH_ASSOC)){
+				$data[] = $row;
+			}
+			// print_r($Users);
+			header('Content-Type: text/csv; charset=utf-8');
+			header('Content-Disposition: attachment; filename='.$table.csv);
+			$output = fopen($directory, 'w');
+			fputcsv($output, self::getTableColumns($db, $table));
+
+			if (count($data) > 0) {
+			    foreach ($data as $row) {
+			        fputcsv($output, $row);
+			    }
 			}
 		}
 	} //class ends
